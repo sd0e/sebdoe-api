af12791556e64607db271daae5c55cde23a78b3b
sebastiandoe5 <sebastiandoe5@gmail.com>
2022-03-18 23:06:23 +0000

    Add lastmod to sitemap

--- index.js
+++ index.js
@@ -23,14 +23,42 @@ http
       firebaseDatabase.ref(db, "/blog/posts"),
       (snapshot) => {
         const first = `
+        <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
-        <url><loc>https://sebdoe.com/</loc></url>
-        <url><loc>https://sebdoe.com/about</loc></url>
-        <url><loc>https://sebdoe.com/category/javascript</loc></url>
-        <url><loc>https://sebdoe.com/category/non-coding</loc></url>
-        <url><loc>https://sebdoe.com/category/programming</loc></url>
-        <url><loc>https://sebdoe.com/category/react</loc></url>
-        <url><loc>https://sebdoe.com/category/gcse</loc></url>
+        <url>
+          <loc>https://sebdoe.com/</loc>
+          <priority>1.0</priority>
+          <changefreq>daily</changefreq>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/about</loc>
+          <priority>0.5</priority>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/category/javascript</loc>
+          <priority>0.2</priority>
+          <changefreq>weekly</changefreq>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/category/non-coding</loc>
+          <priority>0.2</priority>
+          <changefreq>weekly</changefreq>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/category/programming</loc>
+          <priority>0.2</priority>
+          <changefreq>weekly</changefreq>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/category/react</loc>
+          <priority>0.2</priority>
+          <changefreq>weekly</changefreq>
+        </url>
+        <url>
+          <loc>https://sebdoe.com/category/gcse</loc>
+          <priority>0.2</priority>
+          <changefreq>weekly</changefreq>
+        </url>
         `;
 
         const second = `
@@ -39,8 +67,24 @@ http
 
         let middle = [];
         Object.keys(snapshot.val()).forEach((key) => {
+          const postData = snapshot.val()[key];
+
+          const postDate = new Date(Number(postData.date + '000'));
+
+          const parseDateToString = date => {
+            date = date.toString();
+            if (date.length === 2) return date;
+            else return '0' + date;
+          }
+
+          const postDateString = `${postDate.getFullYear()}-${parseDateToString(postDate.getMonth() + 1)}-${parseDateToString(postDate.getDate())}`
+
           middle.push(
-            `\n<url><loc>https://sebdoe.com/article/${key}</loc></url>`
+            `\n<url>
+              <loc>https://sebdoe.com/article/${key}</loc>
+              <lastmod>${postDateString}</lastmod>
+              <priority>0.6</priority>
+            </url>`
           );
         });
 
